c4f31450154ba6e5c212453f820afa57232d77d5
amyjoscelyn <amy.joscelyn@flatironschool.com>
2016-11-17 17:10:05 -0500

    Tracking SKTUtils.  Added camera to scene tracking player, constrained to the map

--- PestControl/PestControl/GameScene.swift
+++ PestControl/PestControl/GameScene.swift
@@ -36,6 +36,9 @@ class GameScene: SKScene
   override func didMove(to view: SKView)
   {
     addChild(player)
+    
+    setupCamera()
+    setupWorldPhysics()
   }
   
   override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?)
@@ -44,5 +47,31 @@ class GameScene: SKScene
     guard let touch = touches.first else { return }
     
     player.move(target: touch.location(in: self))
+  }
+  
+  func setupCamera()
+  {
+    guard let camera = camera, let view = view else { return }
+    
+    let zeroDistance = SKRange(constantValue: 0)
+    let playerConstraint = SKConstraint.distance(zeroDistance, to: player)
+    
+    let xInset = min(view.bounds.width/2 * camera.xScale, background.frame.width/2)
+    let yInset = min(view.bounds.height/2 * camera.yScale, background.frame.height/2)
+    
+    let constraintRect = background.frame.insetBy(dx: xInset, dy: yInset)
+    
+    let xRange = SKRange(lowerLimit: constraintRect.minX, upperLimit: constraintRect.maxX)
+    let yRange = SKRange(lowerLimit: constraintRect.minY, upperLimit: constraintRect.maxY)
+    
+    let edgeConstraint = SKConstraint.positionX(xRange, y: yRange)
+    edgeConstraint.referenceNode = background
+    
+    camera.constraints = [playerConstraint, edgeConstraint] //constraint with highest priority goes last
+  }
+  
+  func setupWorldPhysics()
+  {
+    background.physicsBody = SKPhysicsBody(edgeLoopFrom: background.frame)
   }
 }
